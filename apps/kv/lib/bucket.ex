83129fd3da98aac2a0e80faa31422abacbafb262
R Sánchez <rene.as.himself@gmail.com>
2020-10-25 16:01:36 -0600

    Dependencies and umbrella projects

---
+++ apps/kv/lib/bucket.ex
@@ -0,0 +1,41 @@
+defmodule KV.Bucket do
+  use Agent, restart: :temporary
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  def put(bucket, key, value) do
+    # Here is the clients code
+    Agent.update(bucket, fn state ->
+      # Here is the server code
+      Map.put(state, key, value)
+    end)
+
+    # Back to the client code
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, fn dict ->
+      Map.pop(dict, key)
+    end)
+  end
+end
